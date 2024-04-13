65ca579a124ae2a790cf545dee29b3a6d4be2307
wardaarsalanS <164078434+wardaarsalanS@users.noreply.github.com>
2024-04-13 10:34:48 +0500

    To-Do List Application
    The To-Do List Application is a digital tool for organizing tasks efficiently. Users can add, edit, prioritize, and mark tasks as completed within an intuitive interface.
    
    Features:
    
    Task Management:
    
    Add, edit, delete, and mark tasks as completed.
    Categorize tasks for better organization.
    Prioritization and Sorting:
    
    Prioritize tasks based on urgency.
    Sort tasks by due date, priority, or category.
    Reminders and Notifications:
    
    Set reminders for upcoming tasks.
    Receive notifications for pending tasks.
    User Authentication and Data Persistence:
    
    Secure login ensures data privacy.
    Task data is stored persistently for easy access.
    Implementation:
    Built using web technologies for the front end and Node.js/Python with frameworks like Flask or Django for the backend. Data is stored in databases such as MongoDB or PostgreSQL.
    
    Benefits:
    
    Enhances organization and productivity.
    Customizable to individual preferences.
    Facilitates efficient time management.
    The To-Do List Application is a valuable tool for individuals and teams seeking to streamline task management and boost productivity.

---
+++ listmain.js
@@ -0,0 +1,55 @@
+#!usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+async function createTodo(arr) {
+    let exit = false;
+    do {
+        let ans = await inquirer.prompt([{
+                type: "list",
+                message: "Select an operation",
+                name: "select",
+                choices: ["Add todos item", "Delete todos item", "Update list", "View your list", "Exit"],
+            }]);
+        if (ans.select == "Add todos item") {
+            let addTodo = await inquirer.prompt({
+                type: "input",
+                message: "add item..",
+                name: "todo",
+            });
+            todos.push(addTodo.todo);
+            console.log(todos);
+        }
+        else if (ans.select == "Delete todos item") {
+            let deleteTodo = await inquirer.prompt({
+                type: "list",
+                message: "select item to delete",
+                name: "select",
+                choices: todos,
+            });
+            todos = todos.filter(val => val !== deleteTodo.select);
+            console.log(todos);
+        }
+        else if (ans.select == "Update list") {
+            let updateTodo = await inquirer.prompt({
+                type: "list",
+                message: "select item for update",
+                name: "select",
+                choices: todos,
+            });
+            let newTodo = await inquirer.prompt({
+                type: "input",
+                message: "add new item..",
+                name: "todo",
+            });
+            todos = todos.filter(val => val !== updateTodo.select);
+            todos.push(newTodo.todo);
+            console.log(todos);
+        }
+        else if (ans.select == "View your list") {
+            console.log(todos);
+        }
+        else if (ans.select == "Exit") {
+            exit = true;
+        }
+    } while (!exit);
+}
